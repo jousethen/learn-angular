feb852e49109f0d71458de2169f9588da7300979
jousethen <jthen93@gmail.com>
2023-03-09 00:34:25 -0500

    About me page and routing sample

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { EmployeeContainerComponent } from './components/employee-container/employee-container.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { EmployeeComponent } from './components/employee/employee.component';
+import { AboutComponent } from './components/about/about.component';
 
+const appRoutes: Routes = [
+  { path: '', component: EmployeeContainerComponent },
+  { path: 'about', component: AboutComponent },
+]
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     EmployeeContainerComponent,
     EmployeeComponent,
+    AboutComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
